5c1ebe94669ec3f59b9355092f3a5780d91fe3cb
dnfebri <dwinofebri@gmail.com>
2023-04-14 14:32:21 +0700

    update user controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,17 +3,109 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\RoleUser;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function getAll()
     {
+        if (!auth()->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $roleUser = RoleUser::where('user_id', auth()->user()->id)->first();
+        if ($roleUser['role_id'] !== 1) {
+            return response()->json(['error' => 'Not Access'], 402);
+        }
         $user = User::all();
         return response()->json([
             'message' => '',
             'data' => $user
         ], 200);
+    }
+
+    public function getById($key)
+    {
+        if (!auth()->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $roleUser = RoleUser::where('user_id', auth()->user()->id)->first();
+        if ($roleUser['role_id'] !== 1) {
+            return response()->json(['error' => 'Not Access'], 402);
+        }
+        $user = User::where('id', $key)->first();
+        if (!$user) {
+            return response()->json(['error' => 'User Not found'], 401);
+        }
+        return response()->json([
+            'message' => '',
+            'data' => $user
+        ], 200);
+    }
+
+    public function create(Request $request)
+    {   
+        if (!auth()->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $roleUser = RoleUser::where('user_id', auth()->user()->id)->first();
+        if ($roleUser['role_id'] !== 1) {
+            return response()->json(['error' => 'Not Access'], 402);
+        }
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:3',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->messages());
+        }
+
+        try {
+            $user = User::create([
+                'name' => $request['name'],
+                'email' => $request['email'],
+                'password' => Hash::make($request['password'])
+            ]);
+            return response()->json([
+                'message' => 'pendaftaran berhasil',
+                'data' => $user
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th], 401);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {   
+        if (!auth()->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $roleUser = RoleUser::where('user_id', auth()->user()->id)->first();
+        if ($roleUser['role_id'] !== 1) {
+            return response()->json(['error' => 'Not Access'], 402);
+        }
+        $user = User::where('id', $id)->first();
+        if (!$user) {
+            return response()->json(['error' => 'User Not found'], 401);
+        }
+
+        try {
+            User::where('id', $user->id)
+                ->update([
+                'name' => $request['name'],
+                'email' => $request['email']
+                // 'password' => Hash::make($request['password'])
+            ]);
+            return response()->json([
+                'message' => 'pendaftaran berhasil',
+                'data' => $user
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th], 401);
+        }
     }
 }
--- routes/api/user.php
+++ routes/api/user.php
@@ -3,4 +3,9 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Route::middleware('api')->group(function () {
+// });
 Route::get('/users', [UserController::class, 'getAll']);
+Route::get('/users/{key}', [UserController::class, 'getById']);
+Route::post('/users', [UserController::class, 'create']);
+Route::put('/users/{id}', [UserController::class, 'update']);
